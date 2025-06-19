b633b1324042d0c838302bb747c1cc19deffd7b9
estreg <gerstmann.p@googlemail.com>
2025-06-19 15:26:20 +0200

    build the structure for a list of messages as the agent does its tasks in a loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -16,11 +17,22 @@ def main():
     
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    generate_content(client, messages)
+    
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_prompt,
+        model='gemini-2.0-flash-001',
+        contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") // could be useful later
+    # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
