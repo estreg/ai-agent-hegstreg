b3b226e93629e093e5e3061536c906d071f4b469
estreg <gerstmann.p@googlemail.com>
2025-06-20 16:40:55 +0200

    tests prittified

--- tests.py
+++ tests.py
@@ -1,5 +1,15 @@
 from functions.get_file_content import get_file_content
+def test():
+    
+    result = get_file_content("calculator", "main.py")
+    print(result)
+    
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+    
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+    
+if __name__ == "__main__":
+    test()
 
-print(get_file_content("calculator", "main.py"))
-print(get_file_content("calculator", "pkg/calculator.py"))
-print(get_file_content("calculator", "/bin/cat"))
