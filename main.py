b35227878f5b108fef4bd1048447db70f99164e5
estreg <gerstmann.p@googlemail.com>
2025-06-23 12:35:53 +0200

    build connection for a list of functions and the ai api

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ from google import genai
 from google.genai import types
 from config import system_prompt
 
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 def main():
     load_dotenv()
     
@@ -32,18 +53,26 @@ def main():
     ]
     
     generate_content(client, messages, verbose)
+
+
     
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+             tools=[available_functions], system_instruction=system_prompt),
     )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print("Response:")
+        print(response.text)
+    
 
 
 if __name__ == "__main__":
